7154eecfd4b1cd36743cd00b98bd63989a30c09e
paddygriffin <paddygriffin1995@gmail.com>
2016-04-20 20:47:37 +0100

    JSON
    Changing the phpscripts to json format in order to populate the database
    on the app

---
+++ jsonScripts/Ionic407sun.php
@@ -0,0 +1,30 @@
+
+			<?php		
+				header("Access-Control-Allow-Headers: Content-Type");
+				header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+				header("Access-Control-Allow-Origin: *");
+			
+				$host = "paddygriffin.cloudapp.net";
+				$host1 = "http://paddygriffin.cloudapp.net";
+				$user = "paddygriffin123";
+				$password = "password123";
+				$database = "bus_timetable";				
+				
+				$query = "SELECT * FROM `407_sunday`";
+				
+				//$connect = mysqli_connect($host,$user,$password,$database) or die("Problem connecting.");
+				//$connection = mysql_connect($hostname, $username, $password);
+				$connect = mysqli_connect($host,$user,$password,$database) or die("Problem connecting.");
+				$result = mysqli_query($connect,$query) or die("Bad Query.");
+
+				mysqli_close($connect);
+
+				
+				while($row = mysqli_fetch_assoc($result))
+				{
+				
+					$test[] = $row;
+				}
+				print json_encode($test);
+			?>
+
