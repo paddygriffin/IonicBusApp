f78e8b63a465e9a51db262a84566f06a8e9986c2
paddygriffin <paddygriffin1995@gmail.com>
2016-04-21 16:42:55 +0100

    Completing the timetable
    Adding the rest of the timetable. Time tables are structured using a
    html table

--- bus/www/js/controllers.js
+++ bus/www/js/controllers.js
@@ -58,23 +58,80 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('404Ctrl', function($scope) {
+.controller('404Ctrl', function($scope, $http) {
+	$scope.newTask = function() {
+        $http({
+            method: 'GET',
+            url: 'http://paddygriffin.cloudapp.net/ionic404.php',
+        }).success(function(data) {
+            $scope.categories = data;
+        })
+          .error(function(data) {
+            $scope.categories = "No categories found by that name";
+        })
+    };
 
 })
    
-.controller('405Ctrl', function($scope) {
+.controller('405Ctrl', function($scope, $http) {
+$scope.newTask = function() {
+        $http({
+            method: 'GET',
+            url: 'http://paddygriffin.cloudapp.net/ionic405.php',
+        }).success(function(data) {
+            $scope.categories = data;
+        })
+          .error(function(data) {
+            $scope.categories = "No categories found by that name";
+        })
+    };
 
 })
    
-.controller('407Ctrl', function($scope) {
+.controller('407Ctrl', function($scope, $http) {
+$scope.newTask = function() {
+        $http({
+            method: 'GET',
+            url: 'http://paddygriffin.cloudapp.net/ionic407.php',
+        }).success(function(data) {
+            $scope.categories = data;
+        })
+          .error(function(data) {
+            $scope.categories = "No categories found by that name";
+        })
+    };
 
 })
    
-.controller('409Ctrl', function($scope) {
+.controller('409Ctrl', function($scope, $http) {
+
+$scope.newTask = function() {
+        $http({
+            method: 'GET',
+            url: 'http://paddygriffin.cloudapp.net/ionic409.php',
+        }).success(function(data) {
+            $scope.categories = data;
+        })
+          .error(function(data) {
+            $scope.categories = "No categories found by that name";
+        })
+    };
 
 })
    
-.controller('410Ctrl', function($scope) {
+.controller('410Ctrl', function($scope, $http) {
+
+$scope.newTask = function() {
+        $http({
+            method: 'GET',
+            url: 'http://paddygriffin.cloudapp.net/ionic410.php',
+        }).success(function(data) {
+            $scope.categories = data;
+        })
+          .error(function(data) {
+            $scope.categories = "No categories found by that name";
+        })
+    };
 
 })
  
